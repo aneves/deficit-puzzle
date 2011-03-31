d4153cd912e9c7ef8891b137681f60c9bb81b7d2
aneves <drakferion@gmail.com>
2011-03-31 21:52:40 +0100

    New, blank app. PostgreSQL.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeficitPuzzle::Application.config.secret_token = 'c4c65049795ffc07e21cf28abcc6065585efda79e1346fe95dc1649b8ad0125106ed42d6d7fa3b3023bc725a962d870e02b76e603a24e262d1569d93f6001338'
