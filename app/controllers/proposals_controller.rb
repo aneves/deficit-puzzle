7292404a558b2518b2c14fa7df47b26007c4486e
aneves <drakferion@gmail.com>
2011-04-02 02:20:39 +0100

    Controller cleanup.

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,35 +1,17 @@
 class ProposalsController < ApplicationController
   # GET /proposals
-  # GET /proposals.xml
   def index
     @proposals = Proposal.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @proposals }
-    end
   end
 
   # GET /proposals/1
-  # GET /proposals/1.xml
   def show
     @proposal = Proposal.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @proposal }
-    end
   end
 
   # GET /proposals/new
-  # GET /proposals/new.xml
   def new
     @proposal = Proposal.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @proposal }
-    end
   end
 
   # GET /proposals/1/edit
@@ -38,46 +20,32 @@ class ProposalsController < ApplicationController
   end
 
   # POST /proposals
-  # POST /proposals.xml
   def create
     @proposal = Proposal.new(params[:proposal])
 
-    respond_to do |format|
-      if @proposal.save
-        format.html { redirect_to(@proposal, :notice => 'Proposal was successfully created.') }
-        format.xml  { render :xml => @proposal, :status => :created, :location => @proposal }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @proposal.errors, :status => :unprocessable_entity }
-      end
+    if @proposal.save
+      redirect_to @proposal, :notice => 'Proposal was successfully created.'
+    else
+      render :action => "new"
     end
   end
 
   # PUT /proposals/1
-  # PUT /proposals/1.xml
   def update
     @proposal = Proposal.find(params[:id])
 
-    respond_to do |format|
-      if @proposal.update_attributes(params[:proposal])
-        format.html { redirect_to(@proposal, :notice => 'Proposal was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @proposal.errors, :status => :unprocessable_entity }
-      end
+    if @proposal.update_attributes(params[:proposal])
+      redirect_to @proposal, :notice => 'Proposal was successfully updated.'
+    else
+      render :action => "edit"
     end
   end
 
   # DELETE /proposals/1
-  # DELETE /proposals/1.xml
   def destroy
     @proposal = Proposal.find(params[:id])
     @proposal.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(proposals_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to proposals_url
   end
 end
