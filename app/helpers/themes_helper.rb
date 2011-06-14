0c7238dba7af18077578a6bfcfd37603184badcf
aneves <drakferion@gmail.com>
2011-06-14 20:51:59 +0200

    Undid centralization. Now suspends/approves again.
    Stupid centralization did not work as intended...
    I'm not doing something properly...
    Same old excuse: no internet, can't go look how to do it.

--- app/helpers/themes_helper.rb
+++ app/helpers/themes_helper.rb
@@ -1,10 +1,2 @@
 module ThemesHelper
-  def approve_button_for theme
-    if theme.approved
-    then
-      link_to "suspend", suspend_admin_theme_path(theme), :class => 'button'
-    else
-      link_to "approve", approve_admin_theme_path(theme), :class => 'button'
-    end
-  end
 end
